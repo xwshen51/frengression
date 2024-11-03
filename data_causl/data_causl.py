fea076560f0687ce0f1fd1723b2a8f330e5429ed
linyingyang <yanglinying1024@gmail.com>
2024-11-03 19:41:40 +0000

    enable continuous treatment

--- data_causl/data_causl.py
+++ data_causl/data_causl.py
@@ -11,7 +11,7 @@ import warnings
 warnings.filterwarnings("ignore", message="R is not initialized by the main thread")
 
 
-def generate_data_causl(n=10000, nI = 3, nX= 1, nO = 1, nS = 1, ate = 2, beta_cov = 0, strength_instr = 3, strength_conf = 1, strength_outcome = 1):
+def generate_data_causl(n=10000, nI = 3, nX= 1, nO = 1, nS = 1, ate = 2, beta_cov = 0, strength_instr = 3, strength_conf = 1, strength_outcome = 1, binary_intervention=True):
     # Function to suppress R output
     @contextlib.contextmanager
     def suppress_r_output():
@@ -23,7 +23,7 @@ def generate_data_causl(n=10000, nI = 3, nX= 1, nO = 1, nS = 1, ate = 2, beta_co
     with suppress_r_output():
         robjects.r['source'](r'data_causl/data_causl.R')
         generate_data = robjects.globalenv['data.causl']
-        r_dataframe = generate_data(n=n, nI=nI, nX=nX, nO=nO, nS=nS, ate=ate, beta_cov=beta_cov, strength_instr=strength_instr, strength_conf=strength_conf, strength_outcome=strength_outcome)
+        r_dataframe = generate_data(n=n, nI=nI, nX=nX, nO=nO, nS=nS, ate=ate, beta_cov=beta_cov, strength_instr=strength_instr, strength_conf=strength_conf, strength_outcome=strength_outcome, binary_intervention=binary_intervention)
     # Use the localconverter context manager to convert the R dataframe to a Pandas DataFrame
     with localconverter(robjects.default_converter + pandas2ri.converter):
         df = robjects.conversion.rpy2py(r_dataframe)
